853bf348fc483a2200348308b1f5e159bbb73f65
Унжакова Екатерина <katerina.unz@gmail.com>
2021-09-21 09:01:55 +0700

    Нотификация

--- src/js/search.js
+++ src/js/search.js
@@ -4,6 +4,10 @@ import gallery from '../templates/photo-card.hbs';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
+import '@pnotify/core/dist/BrightTheme.css';
+
+import { alert, notice, info, success, error } from '@pnotify/core';
+
 import InfiniteScroll from 'infinite-scroll';
 
 const { searchForm, btnSearch, galleryRef, btnLoad } = refs;
@@ -12,39 +16,53 @@ const pixabayApiService = new NewApiService();
 
 searchForm.addEventListener('submit', onSearch);
 btnLoad.addEventListener('click', onLoadMore);
-galleryRef.addEventListener('click', e => {
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  e.preventDefault();
-  basicLightbox.create(`<img width="1400" height="900" src="${e.target.dataset.src}">`).show();
-});
+galleryRef.addEventListener('click', onImgClick);
 
 function onSearch(e) {
   e.preventDefault();
+
   pixabayApiService.query = e.currentTarget.elements.query.value;
   if (pixabayApiService.query.length === 0) {
     btnLoad.classList.add('is-hidden');
-
+    alert({
+      title: 'Empty field',
+      text: 'Please, enter word!',
+      delay: 2000,
+      sticker: false,
+    });
     return;
   }
   pixabayApiService.resetPage();
-  pixabayApiService.fetchImgCards().then(hits => {
-    clearCardsContainer();
-    appendCardsMarkup(hits);
-    btnLoad.classList.remove('is-hidden');
-  });
+  pixabayApiService
+    .fetchImgCards()
+    .then(hits => {
+      clearCardsContainer();
+
+      if (hits.length === 0) {
+        error({
+          title: 'Invalid title entered',
+          text: 'Try to enter your request differently',
+          delay: 2000,
+          width: '500px',
+          sticker: false,
+        });
+      } else appendCardsMarkup(hits);
+      btnLoad.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
 }
 
 function onLoadMore() {
-  pixabayApiService.fetchImgCards().then(hits => {
-    appendCardsMarkup(hits);
-    galleryRef.scrollIntoView({
-      behavior: 'smooth',
-      block: 'end',
-    });
-  });
+  pixabayApiService
+    .fetchImgCards()
+    .then(hits => {
+      appendCardsMarkup(hits);
+      galleryRef.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
+    })
+    .catch(onFetchError);
 }
 
 function appendCardsMarkup(hits) {
@@ -55,6 +73,24 @@ function clearCardsContainer() {
   galleryRef.innerHTML = '';
 }
 
+function onImgClick(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  e.preventDefault();
+  basicLightbox.create(`<img width="1400" height="900" src="${e.target.dataset.src}">`).show();
+}
+
+function onFetchError() {
+  error({
+    title: 'Ooooops, something went wrong',
+    text: 'Try again later',
+    delay: 2000,
+    width: '500px',
+    sticker: false,
+  });
+}
 // const infScroll = new InfiniteScroll('.gallery', {
 //   // options
 //   path() {
